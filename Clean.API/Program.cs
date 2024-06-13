9f39dad26e2214be12d272694383537d0f2ab568
SeyedsaeidHosseini <SeyedsaeidHosseini1990@gmail.com>
2024-06-14 00:30:04 +0430

    sync

--- Clean.API/Program.cs
+++ Clean.API/Program.cs
@@ -12,6 +12,11 @@ builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddControllers();
 
 
+
+
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
